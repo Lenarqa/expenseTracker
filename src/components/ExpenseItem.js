bbdfbfd4a97e97268f6cdcc831c16dcfee9251ba
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2021-11-26 23:39:14 +0700

    create card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   const { title, amound, date } = props;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amound}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card";
 
 function Expenses(props) {
   const { expense } = props;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expense[0].title}
         amound={expense[0].amound}
@@ -26,7 +27,7 @@ function Expenses(props) {
         amound={expense[3].amound}
         date={expense[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
