deb3a1642ad472775292a8ab6aed40785e019faa
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2021-11-26 22:25:11 +0700

    create extenseDate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   const { title, amound, date } = props;
-  
-  const month = date.toLocaleString("ru-RU", { month: "long" });
-  const day = date.toLocaleString("ru-RU", { day: "2-digit" });
-  const year = date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amound}</div>
